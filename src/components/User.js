03fa7b16bd3f2bd80cc8ee05bd2486697f38a0b1
harpreet <Harpreet Kour>
2020-08-03 09:48:07 -0600

    changed api link from typicode api to jsonstorage.net/api

--- src/components/User.js
+++ src/components/User.js
@@ -4,11 +4,12 @@ import axios from 'axios'
 function User() {
     const [users, setUsers] = useState([]) 
 
-    useEffect(() => {
-        axios.get(' https://jsonplaceholder.typicode.com/users')
+    useEffect((e) => {
+
+        axios.get('https://jsonstorage.net/api/items/4d56c6a1-9bd8-4795-b714-8b6e815d2edd')
         .then(res => {
             console.log(res)
-            setUsers(res.data)
+            setUsers(res.data.users)
         })
         .catch(err =>{
             console.log(err)
@@ -19,7 +20,7 @@ function User() {
            <ul>
                "Users detail"
     {users.map(user => (
-        <li key={user.id}>Name:{user.name} {"\n"}Username:{user.username}{"\n"}Email:{user.email}{"\n"}Phone:{user.phone}</li>
+        <li key={user.id}>Name:{user.name} {"\n"}Username:{user.username}{"\n"}Email:{user.email}{"\n"}</li>
     ))}
     </ul> 
         </div>
