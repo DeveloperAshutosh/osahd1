5489bc5714b3c854385a356e612de0b94b243f23
harpreet <Harpreet Kour>
2020-08-02 18:55:41 -0600

    added Content.js and User.js under components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
 import UnauthorizedUser from "./unauthorized-user/UnauthorizedUser";
-
+import Content from './Content';
+import User from './User';
 class App extends React.Component {
   render() {
     return (
       <div>
         <p>Hello World</p>
         <UnauthorizedUser />
+        <Content />
+     <User />
       </div>
     );
   }
