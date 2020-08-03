a43b0d16526c9af56aa2e0610bf29f3a411ec426
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-08-02 18:09:40 -0600

    Created news feed branch.

---
+++ src/components/newsfeed/newsfeed.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+
+class NewsFeed extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>News Feed</p>
+        
+      </div>
+    );
+  }
+}
+
+export default NewsFeed;
