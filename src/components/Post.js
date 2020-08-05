b9d1c9bdc0bf80b3f0abfa6249ea8e0127acb395
Osa <osaimola@gmail.com>
2020-08-04 19:26:37 -0600

    connect redux to content.js and posts.js; update delete buttons onlclick function

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,31 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { updatePosts } from ".././actions/social-media-app";
 
 class Post extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
+  render() {
+    return (
+      <>
+        <h3>{this.props.postData.title}</h3>
+        <p>{this.props.postData.body}</p>
 
-        return (
-            <>
-               <h3>{this.props.postData.title}</h3>
-                <p>{this.props.postData.body}</p>
+        <button
+          onClick={() => {
+            console.log("button clicked");
 
-                <button onClick={() => {
-                    console.log("button clicked");
-                }}> Delete</button>
+            let updatedPostList = this.props.someRandomName.posts.filter(
+              (post) => {
+                return post.id !== this.props.postData.id;
+              }
+            );
 
-                <button onClick={() => {
-                    console.log("button clicked");
-                }}>Translate Button</button>
-            </>
-        )
+            this.props.dispatch(updatePosts(updatedPostList));
+          }}
+        >
+          {" "}
+          Delete
+        </button>
 
-    }
-
+        <button
+          onClick={() => {
+            console.log("button clicked");
+          }}
+        >
+          Translate Button
+        </button>
+      </>
+    );
+  }
 }
-export default Post;
 
+function mapStateToProps(state) {
+  return {
+    someRandomName: state,
+  };
+}
 
-
+export default connect(mapStateToProps)(Post);
