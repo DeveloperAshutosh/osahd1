7df40a90b34f4d723536de641eb5f05b79725b65
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-08-05 22:11:39 -0600

    Added posts data to API data, Validation on form fields.

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,102 +1,157 @@
 import React from "react";
 import "./App.css";
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import addNewsFeed from '../actions/NewsFeedActions';
+import Content from "./Content";
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
+import { updatePosts } from '../actions/social-media-app';
+
+
 
 
 class NewsFeed extends React.Component {
 
-  constructor ( props )
-  {
-    super( props );
+  constructor(props) {
+    super(props);
 
     this.state = {
-      newsFeed: "", // Keep track of our new post's value.
-      newsFeedDesc: ""
+
+      // Keep track of our new post's value.
+      newsFeed: "",  //Title of the post.
+      newsFeedDesc: "",  //Description of the post.
+      
     };
   }
 
-
-  // Add a new post title
-  addNewsFeed = ( event ) =>
-  {
-    //event.preventDefault(); // Stop the page from reloading.
- 
-
-    // Dispatch an action; this one we set to require some "newsFeed" text.
-    this.props.dispatch(addNewsFeed(this.state.newsFeed));
-
-    //clear the field for new input.
-   
-    this.updateItem('newsFeed',''); //or use preformatted method.
-  }
-
-   // Add a new post description
-   addNewsFeedDesc = ( event ) =>
-   {
-     //event.preventDefault(); // Stop the page from reloading.
   
- 
-     // Dispatch an action; this one we set to require some "newsFeed" text.
-     this.props.dispatch(addNewsFeed(this.state.newsFeed));
- 
-     //clear the field for new input.
-    
-     this.updateItem('newsFeedDesc',''); //or use preformatted method.
-   }
-
-  updateItem ( key, value )
-  {
+  updateItem(key, value) {
     // We never re-assign the contents of this.state.
     // this.state is ONLY USED FOR READING VALUES, NOT writing.
     // When we need to update anything in state, we need to use this.setState()
     // this.setState() triggers the render() method, so we can see updated state info in our presentation.
-    this.setState( {[key]: value} );
+    this.setState({ [key]: value });
   }
 
-  addPost=(event)=> {
+  // Add a new post title.
+  addNewsFeed = (event) => {
+   
+    // Dispatch an action; this one we set to require "newsFeed" title.
+    this.props.dispatch(addNewsFeed(this.state.newsFeed));
+
+    //clear the field for new input.
+
+    this.updateItem('newsFeed', '');
+  }
+
+  // Add a new post description.
+  addNewsFeedDesc = (event) => {
+
+    // Dispatch an action; this one we set to require some "newsFeed" description.
+    this.props.dispatch(addNewsFeed(this.state.newsFeedDesc));
+
+    //clear the field for new input.
+
+    this.updateItem('newsFeedDesc', ''); 
+  }
+
+  
+  //Function for submit form.
+  handleSubmit = (event) => {
+    event.preventDefault(); //Prevent default load of page.
+  }
+ 
+  //Function on click of submit button("Post Feed").
+  addPost = (event) => {
     event.preventDefault(); // Stop the page from reloading.
 
-    this.props.dispatch(addNewsFeed(`${this.state.newsFeed} ${this.state.newsFeedDesc}`));
+    //Conditions will check if Title and Description fields are empty, it will show an error.
+    
+    if (this.state.newsFeed.trim()==="") 
+    {
+      document.querySelector("#errorTitle").innerHTML ="Title required."; //Error message for Title.
+    
+    }
 
-    console.log(this.state.newsFeed);
-    console.log(this.state.newsFeedDesc);
+    else if (this.state.newsFeedDesc.trim()==="") 
+    {
+      document.querySelector("#errorDesc").innerHTML ="Description required."; //Error message for Title.
+      
+    }
+
+    else if (this.state.newsFeed.trim()!=="" || this.state.newsFeedDesc.trim()!=="")  //Checking if the fields are not empty.
+    {
+      //Fetching data from API.
+      const postData = fetch('https://jsonstorage.net/api/items/f2c563c1-bff6-469b-a954-0dab52edc4c3')
+        .then(posts => {
+          return posts.json();
+        }).then(data => {
+          return data.posts;
+        })
+        //Assign values to data of posts.
+        .then(data => {
+          const newPost = {
+            "id": uuidv4(), // Ensure a unique ID.
+            "title": this.state.newsFeed,
+            "body": this.state.newsFeedDesc
+          };
+
+          const pushData = data;
+          pushData.push(newPost);
+
+          //Using put method to add the new post data to API.
+          axios.put('https://jsonstorage.net/api/items/f2c563c1-bff6-469b-a954-0dab52edc4c3', { "posts": pushData })
+
+          this.props.dispatch(updatePosts(pushData)); //dispatch the updated posts data
+
+        })
+
+        this.state.newsFeed="";
+        this.state.newsFeedDesc="";
+    } 
 
   }
 
   render() {
+
     return (
       <>
-        <h1>News Feed</h1> 
-        <form>
-                <input
-                type="text"
-                name="newsFeed"
-                id="newsFeed"
-                required
-                value={this.state.newsFeed}
-                onChange={event => this.updateItem( 'newsFeed', event.target.value )} placeholder="Title..." />
-                <br /><br />
-              <textarea required placeholder="What's on your mind..." rows="5" cols="25" value={this.state.newsFeedDesc}
-              onChange={event => this.updateItem( 'newsFeedDesc', event.target.value )} />
-               
-               <br /><br />
-            <input type="submit" value="Post Feed" onClick={this.addPost}/>
-          </form>  
-         
+        <button onClick={() => {
+          console.log("button clicked");
+        }}>Logout</button>
+
+        <h1>News Feed</h1>    
+        <form onSubmit={this.handleSubmit}>
+        
+          <input 
+            type="text"
+            name="newsFeed"
+            id="newsFeed"
+            required
+            value={this.state.newsFeed}
+            onChange={event => this.updateItem('newsFeed', event.target.value)} placeholder="Title..." />
+            <div id="errorTitle"></div>
+            <br />
+
+          <textarea id ="newsFeedDesc" required placeholder="What's on your mind..." rows="5" cols="25" value={this.state.newsFeedDesc}
+            onChange={event => this.updateItem('newsFeedDesc', event.target.value)} />
+          <div id="errorDesc"></div>
+          <br />
+          <input type="submit" value="Post Feed" onClick={this.addPost} />
+        </form>
+        <Content />
       </>
-     
-      
     );
-    
+
   }
 }
 
-function mapStateToProps (state) {
+//mapStateToProps function receives the store state 
+function mapStateToProps(state) {
   return {
     store: state,
   };
 }
 
-
-export default connect (mapStateToProps)(NewsFeed) ;
+//mapStateToProps with connect is used for selecting the part of data from the the store that the connected component needs.
+export default connect(mapStateToProps)(NewsFeed);
