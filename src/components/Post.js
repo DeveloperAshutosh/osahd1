a0c148a74fad3e1c51e439c55fe4ecbd2b1af7c5
harpreet <Harpreet Kour>
2020-08-05 20:03:58 -0600

    working on translate features

--- src/components/Post.js
+++ src/components/Post.js
@@ -10,11 +10,6 @@ class Post extends Component {
   //   translated: '...'
   // }
 }
-translate(){
-  axios.post(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=$en&tl=en&dt=t&q={props.postData.title}`)
-  .then(data => {
-    this.setState()
-  }) }
 
   render() {
     return (
@@ -35,6 +30,7 @@ translate(){
             this.props.dispatch(updatePosts(updatedPostList));
              // use a put request to send updated list to our json stroage api after deleting the post.
  
+  
     axios
       .put(
         "https://jsonstorage.net/api/items/f2c563c1-bff6-469b-a954-0dab52edc4c3",
@@ -46,20 +42,24 @@ translate(){
           {" "}
           Delete
         </button>
-       
+        translate(){
+          axios.post(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=$la&tl=en&dt=t&q={}`)
+          .then(data => {
+            this.setState()
+          })
 
-        
+        }
 
         <button
           onClick={() => {
-            {this.translate}
+          
           }}
         >
           Translate Button
         </button>
       </>
     );
-  
+  }
 }
 
 function mapStateToProps(state) {
