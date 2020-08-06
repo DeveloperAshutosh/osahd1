c7ac79562da32d3fb49857e6a0c64ecbde1dcad5
Osa <osaimola@gmail.com>
2020-08-05 21:02:16 -0600

    use google api to translate text [bug: source ans target language become the same]

--- src/components/Post.js
+++ src/components/Post.js
@@ -6,16 +6,59 @@ import axios from "axios";
 class Post extends Component {
   constructor(props) {
     super(props);
-  //   this.state ={
-  //   translated: '...'
-  // }
-}
+    this.state = {
+      title: this.props.postData.title,
+      body: this.props.postData.body,
+      source: "en",
+      target: "fr",
+    };
+  }
+
+  translate() {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://translate.googleapis.com/translate_a/single?client=gtx&sl=${this.state.source}&tl=${this.state.target}&dt=t&q=${this.props.postData.body}`
+      )
+      .then((response) => {
+        return response.data[0];
+      })
+      .then((translatedArray) => {
+        let text = "";
+        for (let translation of translatedArray) {
+          text = text + translation[0];
+        }
+
+        this.setState({ body: text });
+      });
+
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=fr&dt=t&q=${this.props.postData.title}`
+      )
+      .then((response) => {
+        return response.data[0];
+      })
+      .then((translatedArray) => {
+        let text = "";
+        for (let translation of translatedArray) {
+          text = text + translation[0];
+        }
+
+        this.setState({ title: text });
+      });
+
+    let currentLanguage = this.state.source;
+    this.setState({
+      source: currentLanguage === "en" ? "fr" : "en",
+      target: currentLanguage === "en" ? "fr" : "en",
+    });
+  }
 
   render() {
     return (
       <>
-        <h3>{this.props.postData.title}</h3>
-        <p>{this.props.postData.body}</p>
+        <h3>{this.state.title}</h3>
+        <p>{this.state.body}</p>
 
         <button
           onClick={() => {
@@ -28,31 +71,19 @@ class Post extends Component {
             );
 
             this.props.dispatch(updatePosts(updatedPostList));
-             // use a put request to send updated list to our json stroage api after deleting the post.
- 
-  
-    axios
-      .put(
-        "https://jsonstorage.net/api/items/f2c563c1-bff6-469b-a954-0dab52edc4c3",
-        { "posts":updatedPostList }
-      )
- 
+            // use a put request to send updated list to our json stroage api after deleting the post.
+
+            axios.put(
+              "https://jsonstorage.net/api/items/f2c563c1-bff6-469b-a954-0dab52edc4c3",
+              { posts: updatedPostList }
+            );
           }}
         >
-          {" "}
           Delete
         </button>
-        translate(){
-          axios.post(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=$la&tl=en&dt=t&q={}`)
-          .then(data => {
-            this.setState()
-          })
-
-        }
-
         <button
           onClick={() => {
-          
+            this.translate();
           }}
         >
           Translate Button
