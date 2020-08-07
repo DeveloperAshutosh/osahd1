43db1fff411dbaecdd1d1fe385467afad26454a7
harpreet <Harpreet Kour>
2020-08-06 21:20:43 -0600

    made delete button working correctly and now moving to translate button

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,40 +5,38 @@ import { updatePosts } from ".././actions/social-media-app";
 import axios from "axios";
 
 class Content extends Component {
-  constructor(props) {
-    super(props);
-    this.state={
-      title: []
-     
-    };
-  }
-  componentDidMount() {
-    fetch(
-      "https://jsonstorage.net/api/items/f2c563c1-bff6-469b-a954-0dab52edc4c3"
-    )
-      .then((posts) => {
-        return posts.json();
-      })
-      .then((data) => {
-        let title=data.posts;
-        this.setState({ title: title });
-        this.props.dispatch(updatePosts(title));
-      });
-   
 
-  }
-  render() {
-    return (
-      <>
-        <button
-          onClick={() => {
-            console.log("button clicked");
-          }}
-        >
-          Refresh
-        </button>
-
-        <div className="container1">
+constructor(props) {
+  super(props);
+  this.state = {
+    title: []
+  };
+}
+componentDidMount() {
+  fetch(
+    "https://jsonstorage.net/api/items/f2c563c1-bff6-469b-a954-0dab52edc4c3"
+  )
+    .then((posts) => {
+      return posts.json();
+    })
+    .then((data) => {
+      let title = data.posts;
+      this.setState({ title: title });
+      this.props.dispatch(updatePosts(title));
+      // use a put request to send updated list to our json stroage api
+    });
+}
+render() {
+  return (
+    <>
+      <button
+        onClick={() => {
+          console.log("button clicked");
+        }}
+      >
+        Refresh
+      </button>
+      <div className="container1">
           {this.props.someRandomName.posts.map((pic) => {
             return <Post postData={pic} />;
           })}
@@ -48,9 +46,20 @@ class Content extends Component {
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    someRandomName: state,
-  };
-}
-export default connect(mapStateToProps)(Content);
+  function mapStateToProps(state) {
+    return {
+      someRandomName: state,
+    };
+  }
+  export default connect(mapStateToProps)(Content);
+
+
+
+
+
+
+
+
+
+
+
--- src/components/Post.js
+++ src/components/Post.js
@@ -4,60 +4,17 @@ import { updatePosts } from ".././actions/social-media-app";
 import axios from "axios";
 
 class Post extends Component {
+
+
   constructor(props) {
     super(props);
-    this.state = {
-      title: this.props.postData.title,
-      body: this.props.postData.body,
-      source: "en",
-      target: "fr",
-    };
-  }
-
-  translate() {
-    axios
-      .get(
-        `https://cors-anywhere.herokuapp.com/https://translate.googleapis.com/translate_a/single?client=gtx&sl=${this.state.source}&tl=${this.state.target}&dt=t&q=${this.props.postData.body}`
-      )
-      .then((response) => {
-        return response.data[0];
-      })
-      .then((translatedArray) => {
-        let text = "";
-        for (let translation of translatedArray) {
-          text = text + translation[0];
-        }
-
-        this.setState({ body: text });
-      });
-
-    axios
-      .get(
-        `https://cors-anywhere.herokuapp.com/https://translate.googleapis.com/translate_a/single?client=gtx&sl=${this.state.source}&tl=${this.state.target}&dt=t&q=${this.props.postData.title}`
-      )
-      .then((response) => {
-        return response.data[0];
-      })
-      .then((translatedArray) => {
-        let text = "";
-        for (let translation of translatedArray) {
-          text = text + translation[0];
-        }
-
-        this.setState({ title: text });
-      });
-
-    this.setState({
-      source: this.state.source === "en" ? "fr" : "en",
-      target: this.state.target === "en" ? "fr" : "en",
-    });
   }
 
   render() {
     return (
       <>
-        <h3>{this.state.title}</h3>
-        <p>{this.state.body}</p>
+        <h3>{this.props.postData.title}</h3>
+        <p>{this.props.postData.body}</p>
 
         <button
           onClick={() => {
@@ -70,19 +27,22 @@ class Post extends Component {
             );
 
             this.props.dispatch(updatePosts(updatedPostList));
-            // use a put request to send updated list to our json stroage api after deleting the post.
+              // use a put request to send updated list to our json stroage api after deleting the post.
 
-            axios.put(
-              "https://jsonstorage.net/api/items/f2c563c1-bff6-469b-a954-0dab52edc4c3",
-              { posts: updatedPostList }
-            );
+    axios
+    .put(
+      "https://jsonstorage.net/api/items/f2c563c1-bff6-469b-a954-0dab52edc4c3",
+      { "posts":updatedPostList }
+    )
           }}
         >
+          
           Delete
         </button>
+
         <button
           onClick={() => {
-            this.translate();
+            console.log("button clicked");
           }}
         >
           Translate Button
@@ -91,7 +51,6 @@ class Post extends Component {
     );
   }
 }
-
 function mapStateToProps(state) {
   return {
     someRandomName: state,
@@ -99,3 +58,23 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps)(Post);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
