e645bb7403a31178f1a25eaeafd48e53ae394fb8
Osa <osaimola@gmail.com>
2020-08-07 13:40:27 -0600

    fix logout bug

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,33 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
-import { setLoggedIn , setCurrentUser } from ".././actions/social-media-app";
+import { setLoggedIn, setCurrentUser } from ".././actions/social-media-app";
 
 //Nav displays the menu links on the page.
 
 class Nav extends React.Component {
-  logout = () =>{
-   
-    
+  logout() {
     this.props.dispatch(setLoggedIn(false));
     this.props.dispatch(setCurrentUser(null));
-    console.log(setLoggedIn);
-
   }
-  
 
-  
   render() {
     return (
       <nav>
         <ul>
           <li>
-          <Link to="/HomePage">Home Page</Link>
+            <Link to="/HomePage">Home Page</Link>
             <Link to="/NewsFeed">News Feed</Link>
             <Link to="/Search">Search Page</Link>
-           </li>
-           <button onClick =  {() =>this.logout} > Log Out</button>
-          
+          </li>
+          <button onClick={() => this.logout()}> Log Out</button>
         </ul>
       </nav>
     );
